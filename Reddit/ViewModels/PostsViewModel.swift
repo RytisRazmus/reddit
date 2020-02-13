dbae000d6fd37e102e7336c4a4c0c638a53ff251
RytisRazmus <rytis.razmus@gmail.com>
2020-02-13 21:38:46 +0200

    added a way to identify videos

--- Reddit/ViewModels/PostsViewModel.swift
+++ Reddit/ViewModels/PostsViewModel.swift
@@ -13,7 +13,7 @@ class PostsViewModel {
     private let jsonParser = JSONParser()
     private let subreddit = "popular"
     private let fetchLimit = 20
-    private let fetcher: DataFetcher
+    private weak var fetcher: DataFetcher?
     private weak var delegate: DataSubscription?
     
     var posts = [PostData]() {
@@ -30,7 +30,7 @@ class PostsViewModel {
     }
     
     func fetchPosts(){
-        fetcher.fetch(urlString: "https://api.reddit.com/r/\(subreddit)?limit=\(fetchLimit)") { [ weak self ] (data, error)  in
+        fetcher?.fetch(urlString: "https://api.reddit.com/r/\(subreddit)?limit=\(fetchLimit)") { [ weak self ] (data, error)  in
             self?.handleFetchResult(data: data, error: error)
         }
     }
@@ -38,7 +38,7 @@ class PostsViewModel {
     func fetchMore(){
         guard let name = posts.last?.name else { return }
         let redditUrlString = "https://api.reddit.com/r/\(subreddit)?&after=\(name)&limit=\(fetchLimit)"
-        fetcher.fetch(urlString: redditUrlString) { [weak self] (data, error) in
+        fetcher?.fetch(urlString: redditUrlString) { [weak self] (data, error) in
             self?.handleFetchResult(data: data, error: error)
         }
     }
@@ -60,11 +60,56 @@ class PostsViewModel {
     private func decodeToPostData(data: Data?) -> NetworkError? {
         guard let data = data else { return .badData }
         if let postsData = jsonParser.decodeToPostData(data: data){
-            postsData.forEach{$0.imageScaledHeight = $0.preview?.images.first?.calculateImageHeight()}
+            postsData.forEach{
+                $0.imageScaledHeight = $0.preview?.images.first?.calculateImageHeight()
+                checkIsVideoOrGif(post: $0)
+            }
             posts += postsData
             return nil
         }
         return .badData
+    }
+    // reddit does not have any node to identify if a post contains a video
+    // they have a node is_video but it's always false
+    // you have to look into the json and figure out the media type
+    // also there's a possability I'm an idiot
+    
+    func checkIsVideoOrGif(post: PostData){
+        if let mp4 = post.preview?.images.first?.variants?.mp4?.source.url {
+            post.videoUrl = mp4.htmlDecoded
+            return
+        }
+        
+        if let fallbackUrl = post.preview?.redditVideo?.fallbackUrl {
+            post.videoUrl = fallbackUrl
+            return
+        }
+        // TODO: ADD youtube video player
+        if let mediaDomainUrl = post.secureMediaEmbed?.mediaDomainUrl {
+            post.videoUrl = mediaDomainUrl
+            return
+        }
+        
+        if let fallbackUrl = post.secureMedia?.redditVideo?.fallbackUrl {
+            post.videoUrl = fallbackUrl
+            return
+        }
+        checkGif(post: post)
+    }
+    
+    // they also have is_gif node but it's not always accurate...
+    
+    private func checkGif(post: PostData){
+        guard let url = post.url else { return }
+        // .gifv is really .mp4
+        if url.contains(".gifv"){
+            post.videoUrl = url.replacingOccurrences(of: ".gifv", with: ".mp4")
+            return
+        }
+        // TODO: add gif player
+        if url.contains(".gif"){
+            return
+        }
     }
     
 }
